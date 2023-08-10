c349311023d6c344b9333e3ca56a490ef4db214e
OtterSloth <76695983+OtterSloth@users.noreply.github.com>
2023-08-10 12:31:38 -0400

    Refactored to use flashCard state object

--- App.jsx
+++ App.jsx
@@ -2,18 +2,18 @@ import React from "react"
 import { nanoid } from "nanoid"
 
 export default function app() {
-    
-	const [flashCard, setFlashCard] = React.useState({
-		question: "What is React?",
-		choices: ["A JavaScript framework", "A JavaScript library"],
-		answer: "A JavaScript library.",
-		explanation: `It's absolutely crucial that if you ever hear anyone have the
+  const [flashCard, setFlashCard] = React.useState({
+    question: "What is React?",
+    choices: ["A JavaScript framework", "A JavaScript library"],
+    answer: "A JavaScript library.",
+    explanation: `It's absolutely crucial that if you ever hear anyone have the
               audacity to call it a framework, you must correct them as
               pedantically as possible, preferably starting your response with
               the phrase "um, actually..."`,
-	})
+    isFlipped: false,
+  })
 
-/* Challenge: 
+  /* Challenge: 
 
     The flashcard needs to flip back and forth. Your task is to set this up as follows: 
     
@@ -27,34 +27,41 @@ export default function app() {
            continue flipping the card back and forth as many times as they want. 
 */
 
-	return (
-		<div>
-			<header>
-				<img src="./images/react.svg" />
-				<h1> React Study Buddy </h1>
-			</header>
+  const handleClick = () => {
+    setFlashCard((prev) => ({ ...prev, isFlipped: !prev.isFlipped }))
+  }
 
-			{/*-------Edit the div below!------------*/}
+  const flashCardClass = flashCard.isFlipped
+    ? "flash-card flipped"
+    : "flash-card"
 
-			<div className="flash-card">
-			
-            {/*-------Edit the div above!------------*/}
+  return (
+    <div>
+      <header>
+        <img src="./images/react.svg" />
+        <h1> React Study Buddy </h1>
+      </header>
 
-				<div className="flash-card-inner">
-					<div className="flash-card-front">
-						<p className="question">{flashCard.question}</p>
-						<ol type="a">
-							{flashCard.choices.map(choice => (
-								<li key={nanoid()}>{choice}</li>
-							))}
-						</ol>
-					</div>
-					<div className="flash-card-back">
-						<p className="answer">{flashCard.answer}</p>
-						<p>{flashCard.explanation}</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
+      {/*-------Edit the div below!------------*/}
+
+      <div className={flashCardClass} onClick={handleClick}>
+        {/*-------Edit the div above!------------*/}
+
+        <div className="flash-card-inner">
+          <div className="flash-card-front">
+            <p className="question">{flashCard.question}</p>
+            <ol type="a">
+              {flashCard.choices.map((choice) => (
+                <li key={nanoid()}>{choice}</li>
+              ))}
+            </ol>
+          </div>
+          <div className="flash-card-back">
+            <p className="answer">{flashCard.answer}</p>
+            <p>{flashCard.explanation}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
